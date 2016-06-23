f6733b33c18a1daa9d55906773d0cc668ee75180
Alexandru Simandi <alex_simi_jr@yahoo.com>
2016-06-23 22:41:54 +0300

    fixed wrapper and added replies

--- speech.js
+++ speech.js
@@ -2,32 +2,44 @@ $(document).ready(function(){
 	setTimeout(function(){
 		executeSamaritan('what are your commands ?');
 //		executeSamaritanWrapper('adsasd')();
-	}, 1000);
+	}, 700);
 
 	var executeSamaritanWrapper = function(msg){
 		return function(){executeSamaritan(msg)};
 	};
 
 	if (annyang) {
-
 		var commands = {
-			'yes': executeSamaritanWrapper('yes?'),
+			'yes': executeSamaritanWrapper('yes what ?'),
+			'no': executeSamaritanWrapper('ok then what is your suggestion ?'),
 			'hello': executeSamaritanWrapper('hello'),
 			'who are you': executeSamaritanWrapper('i am samaritan !'),
-			'turn off': executeSamaritanWrapper('turning off'),
-			'restart': executeSamaritanWrapper('rebooting'),
+			'what are you': executeSamaritanWrapper('i am samaritan !'),
+			'turn off': executeSamaritanWrapper('shutdown initiated'),
+			'restart': executeSamaritanWrapper('initiating reboot sequence'),
 			'who created you': executeSamaritanWrapper("it's irrelevant"),
+			'who won': executeSamaritanWrapper('team machine'),
+			'say my name': executeSamaritanWrapper('heisenberg'),
 			'find :name': function(name){
 				executeSamaritan('searching for ' + name);
 			},
 			'search (for) :name': function(name){
 				executeSamaritan('searching for ' + name);
+			},
+			//repeats words if it's not one of the above
+			':first :second :third': function(first, second, third){
+				executeSamaritan(first + ' ' + second + ' ' + third + ' ?');
+			},
+			':first :second': function(first, second){
+				executeSamaritan(first + ' ' + second + ' ?');
+			},
+			':word': function(word){
+				executeSamaritan(word + ' ?');
 			}
 		};
 
 		annyang.addCommands(commands);
 		annyang.start();
-
 	}
 
 });
